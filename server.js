f50a7d306699025cbff750e8064ddd92f191e257
Nurul Fadilah <150462453+Nufa-nur@users.noreply.github.com>
2024-12-02 23:05:52 +0700

    Update server.js
    change port

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json()); // untuk parse JSON di request body
 // Menyambungkan endpoint /register dengan router auth
 app.use("/", authRoutes);
 
-const PORT = 3000;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
